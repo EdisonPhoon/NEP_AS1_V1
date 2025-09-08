29f1609746e12bd47462a7ac3ffda0252e6127bb
Edison Phoon Tek Wei <24FTT2013@pb.edu.bn>
2025-09-09 07:55:47 +0800

    Initial commit

---
+++ database/seeders/GamesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class GamesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('games')->insert([
+            [
+            'name' => 'Minecraft', 
+            'genre' => 'Sandbox', 
+            'rating' => 7, 
+            'created_at' => now(), 
+            'updated_at' => now()
+            ]
+        ]);
+    }
+}
